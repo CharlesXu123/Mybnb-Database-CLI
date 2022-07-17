3bcbb6b65ce7e90f6c4e187e632a85ffbe1d8f8f
CharlesXu123 <78982364+CharlesXu123@users.noreply.github.com>
2022-07-17 18:06:19 -0400

    added date of birth check

--- mysqlFiles/initialize.sql
+++ mysqlFiles/initialize.sql
@@ -2,19 +2,22 @@ DROP TABLE IF EXISTS Renter;
 
 CREATE TABLE Renter (
     uId char(36) primary key,
-    name varchar(50),
-    address varchar(225),
-    date_of_birth DATE,
-    occupation varchar(225),
-    payment_info varchar(255)
+    name varchar(50) not null ,
+    address varchar(225) not null ,
+    date_of_birth DATE not null,
+    occupation varchar(225) not null ,
+    payment_info varchar(255) not null
 );
 
-CREATE TRIGGER generate_Renter_uId
+CREATE TRIGGER renter_trigger
     BEFORE INSERT ON Renter
     FOR EACH ROW
 BEGIN
     IF new.uId IS NULL THEN
         SET new.uId = uuid();
+    END IF;
+    IF (YEAR(CURDATE()) - YEAR(new.date_of_birth)) < 18 THEN
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'need be at least 18 years old!';
     END IF;
 END;
 
@@ -22,17 +25,20 @@ DROP TABLE IF EXISTS Host;
 
 CREATE TABLE Host (
                         uId char(36) primary key,
-                        name varchar(50),
-                        address varchar(225),
-                        date_of_birth DATE,
-                        occupation varchar(225)
+                        name varchar(50) not null ,
+                        address varchar(225) not null ,
+                        date_of_birth DATE not null,
+                        occupation varchar(225) not null
 );
 
-CREATE TRIGGER generate_Host_uId
+CREATE TRIGGER host_trigger
     BEFORE INSERT ON Host
     FOR EACH ROW
 BEGIN
     IF new.uId IS NULL THEN
         SET new.uId = uuid();
+    END IF;
+    IF (YEAR(CURDATE()) - YEAR(new.date_of_birth)) < 18 THEN
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'need be at least 18 years old!';
     END IF;
 END;
---
+++ mysqlFiles/test.sql
@@ -0,0 +1,3 @@
+INSERT INTO Renter (name, address, date_of_birth, occupation)
+VALUES
+    ('Genevieve Mcintyre','P.O. Box 676, 8686 Vitae, Road','2000-05-29','Vestibulum Incorporated');
